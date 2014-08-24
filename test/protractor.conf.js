3cb5fd20149b3b82fec436f229dd740a5d4006ff
kmsheng <kmsh3ng@gmail.com>
2014-08-25 00:07:51 +0800

    update protractor config

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,12 +1,18 @@
 // An example configuration file.
 exports.config = {
+
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
   // The address of a running selenium server.
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.42.2.jar', // Make use you check the version in the folder
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER
   },
 
   // Spec patterns are relative to the current working directly when
